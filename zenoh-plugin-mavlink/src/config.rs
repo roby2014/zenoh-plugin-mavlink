d16082e4e1b9cba99a6b6fb23dde71531d64663c
roby2014 <roby2x14@gmail.com>
2024-09-24 10:03:25 +0100

    zenoh: update to 1.0.0

--- zenoh-plugin-mavlink/src/config.rs
+++ zenoh-plugin-mavlink/src/config.rs
@@ -1,7 +1,11 @@
 use serde::Deserialize;
-use zenoh::prelude::OwnedKeyExpr;
+use zenoh::key_expr::OwnedKeyExpr;
 
 use crate::mavlink_connection::MAVLinkConnection;
+
+pub const DEFAULT_NODENAME: &str = "zenoh_bridge_mavlink";
+pub const DEFAULT_WORK_THREAD_NUM: usize = 2;
+pub const DEFAULT_MAX_BLOCK_THREAD_NUM: usize = 50;
 
 #[derive(Deserialize, Debug)]
 #[serde(deny_unknown_fields)]
@@ -10,6 +14,16 @@ pub struct Config {
     pub mavlink_connections: Vec<MAVLinkConnection>,
     #[serde(default)]
     pub broadcast_channel_capacity: usize,
-    #[serde(default)]
-    pub group_member_id: Option<OwnedKeyExpr>,
+    #[serde(default = "default_work_thread_num")]
+    pub work_thread_num: usize,
+    #[serde(default = "default_max_block_thread_num")]
+    pub max_block_thread_num: usize,
+}
+
+fn default_work_thread_num() -> usize {
+    DEFAULT_WORK_THREAD_NUM
+}
+
+fn default_max_block_thread_num() -> usize {
+    DEFAULT_MAX_BLOCK_THREAD_NUM
 }
--- zenoh-plugin-mavlink/src/lib.rs
+++ zenoh-plugin-mavlink/src/lib.rs
@@ -1,27 +1,59 @@
+use std::future::Future;
+use std::sync::atomic::{AtomicUsize, Ordering};
 use std::sync::Arc;
 
+use liveliness::{ke_liveliness_plugin, ke_liveliness_pub, ke_liveliness_sub};
 use protocol::Protocol;
-use tokio::task::JoinSet;
+use tokio::task::{JoinHandle, JoinSet};
 use tracing::{debug, error, info};
 use tracing::{info_span, Instrument};
-use zenoh::ke;
-use zenoh::liveliness::LivelinessToken;
-use zenoh::prelude::r#async::AsyncResolve;
-use zenoh::prelude::*;
-use zenoh::Result as ZResult;
 use zenoh::{
-    plugins::{RunningPlugin, RunningPluginTrait, ZenohPlugin},
-    runtime::Runtime,
+    internal::{
+        plugins::{RunningPlugin, RunningPluginTrait, ZenohPlugin},
+        runtime::Runtime,
+        zerror,
+    },
+    key_expr::format::keformat,
+    liveliness::LivelinessToken,
+    Result as ZResult, Session,
 };
 use zenoh_plugin_trait::{plugin_long_version, plugin_version, Plugin, PluginControl};
 
 pub mod config;
+pub mod liveliness;
 pub mod mavlink_connection;
 pub mod protocol;
 use config::Config;
-use zenoh_util::core::zerror;
 
-const KE_PREFIX_LIVELINESS_GROUP: &str = "zenoh-plugin-mavlink";
+lazy_static::lazy_static! {
+    static ref WORK_THREAD_NUM: AtomicUsize = AtomicUsize::new(config::DEFAULT_WORK_THREAD_NUM);
+    static ref MAX_BLOCK_THREAD_NUM: AtomicUsize = AtomicUsize::new(config::DEFAULT_MAX_BLOCK_THREAD_NUM);
+    // The global runtime is used in the dynamic plugins, which we can't get the current runtime
+    static ref TOKIO_RUNTIME: tokio::runtime::Runtime = tokio::runtime::Builder::new_multi_thread()
+               .worker_threads(WORK_THREAD_NUM.load(Ordering::SeqCst))
+               .max_blocking_threads(MAX_BLOCK_THREAD_NUM.load(Ordering::SeqCst))
+               .enable_all()
+               .build()
+               .expect("Unable to create runtime");
+}
+#[inline(always)]
+pub(crate) fn spawn_runtime<F>(task: F) -> JoinHandle<F::Output>
+where
+    F: Future + Send + 'static,
+    F::Output: Send + 'static,
+{
+    // Check whether able to get the current runtime
+    match tokio::runtime::Handle::try_current() {
+        Ok(rt) => {
+            // Able to get the current runtime (standalone binary), spawn on the current runtime
+            rt.spawn(task)
+        }
+        Err(_) => {
+            // Unable to get the current runtime (dynamic plugins), spawn on the global runtime
+            TOKIO_RUNTIME.spawn(task)
+        }
+    }
+}
 
 pub struct MAVLinkPlugin;
 
@@ -39,7 +71,7 @@ impl Plugin for MAVLinkPlugin {
         // Try to initiate login.
         // Required in case of dynamic lib, otherwise no logs.
         // But cannot be done twice in case of static link.
-        zenoh_util::try_init_log_from_env();
+        zenoh::try_init_log_from_env();
 
         let runtime_conf = runtime.config().lock();
         let plugin_conf = runtime_conf
@@ -48,12 +80,18 @@ impl Plugin for MAVLinkPlugin {
         info!("{:?}", plugin_conf.clone());
         let config: Config = serde_json::from_value(plugin_conf.clone())
             .map_err(|e| zerror!("Plugin `{}` configuration error: {}", name, e))?;
-        async_std::task::spawn(run(runtime.clone(), config));
+        WORK_THREAD_NUM.store(config.work_thread_num, Ordering::SeqCst);
+        MAX_BLOCK_THREAD_NUM.store(config.max_block_thread_num, Ordering::SeqCst);
+
+        spawn_runtime(run(runtime.clone(), config));
         Ok(Box::new(MAVLinkPlugin))
     }
 }
 
 impl RunningPluginTrait for MAVLinkPlugin {}
+
+#[cfg(feature = "dynamic_plugin")]
+zenoh_plugin_trait::declare_plugin!(MAVLinkPlugin);
 
 pub async fn run(runtime: Runtime, config: Config) {
     debug!(
@@ -63,54 +101,47 @@ pub async fn run(runtime: Runtime, config: Config) {
     debug!("Zenoh MAVLink plugin {:?}", config);
 
     // open zenoh-net Session
-    let zsession = match zenoh::init(runtime).res_async().await {
+    let zsession = match zenoh::session::init(runtime).await {
         Ok(session) => Arc::new(session),
         Err(e) => {
-            error!("Unable to init zenoh session for ZeroMQ plugin: {e:?}");
+            error!("Unable to init zenoh session for MAVLink plugin: {e:?}");
             return;
         }
     };
 
-    // create group member using the group_member_id if configured, or the Session ID otherwise
-    let member_id = match config.group_member_id {
-        Some(ref id) => id.clone(),
-        None => zsession.zid().into_keyexpr(),
-    };
-    let member = match zsession
-        .liveliness()
-        .declare_token(
-            keyexpr::new(KE_PREFIX_LIVELINESS_GROUP).unwrap() / &zsession.zid().into_keyexpr(),
-        )
-        .res_async()
-        .await
-    {
+    // Declare plugin's liveliness token
+    let ke_liveliness = keformat!(
+        ke_liveliness_plugin::formatter(),
+        zenoh_id = zsession.zid().into_keyexpr()
+    )
+    .unwrap();
+    let member = match zsession.liveliness().declare_token(ke_liveliness).await {
         Ok(member) => member,
         Err(e) => {
-            error!("Unable to declare liveliness token for ZeroMQ plugin: {e:?}");
+            tracing::error!(
+                "Unable to declare liveliness token for MAVLink plugin : {:?}",
+                e
+            );
             return;
         }
     };
 
     let mav_plugin = MAVLinkPluginRuntime {
-        config,
-        zsession: &zsession,
+        config: Arc::new(config),
+        zsession,
         _member: member,
-        _member_id: member_id,
     };
 
     mav_plugin.run().await;
 }
 
-pub(crate) struct MAVLinkPluginRuntime<'a> {
-    config: Config,
-    // Note: &'a Arc<Session> here to keep the ownership of Session outside this struct
-    // and be able to store the publishers/subscribers it creates in this same struct.
-    zsession: &'a Arc<Session>,
-    _member: LivelinessToken<'a>,
-    _member_id: OwnedKeyExpr,
+pub(crate) struct MAVLinkPluginRuntime {
+    config: Arc<Config>,
+    zsession: Arc<Session>,
+    _member: LivelinessToken,
 }
 
-impl<'a> MAVLinkPluginRuntime<'a> {
+impl MAVLinkPluginRuntime {
     async fn run(&self) {
         // spawn broadcast channel
         let (tx, mut rx) =
@@ -126,17 +157,13 @@ impl<'a> MAVLinkPluginRuntime<'a> {
         // launch task to handle outgoing data for the zenoh network
         let zsession = self.zsession.clone();
         tokio::spawn(async move {
-            let publisher = zsession
-                .declare_publisher(ke!("mavlink/out"))
-                .res_async()
-                .await
-                .unwrap();
+            let ke = keformat!(ke_liveliness_pub::formatter(), zenoh_id = "*",).unwrap();
+            let publisher = zsession.declare_publisher(ke).await.unwrap();
 
             match rx.recv().await {
                 Ok(msg) => {
                     publisher
                         .put("TODO: publish mavlink message")
-                        .res_async()
                         .await
                         .unwrap();
                     debug!("forwarded message from broadcast channel to zenoh");
@@ -153,11 +180,8 @@ impl<'a> MAVLinkPluginRuntime<'a> {
         // launch task to handle incoming data for the zenoh network
         let zsession = self.zsession.clone();
         tokio::spawn(async move {
-            let subscriber = zsession
-                .declare_subscriber(ke!("mavlink/in"))
-                .res_async()
-                .await
-                .unwrap();
+            let ke = keformat!(ke_liveliness_sub::formatter(), zenoh_id = "*",).unwrap();
+            let subscriber = zsession.declare_subscriber(ke).await.unwrap();
 
             while let Ok(sample) = subscriber.recv_async().await {
                 debug!("received message from zenoh");
